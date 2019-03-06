df0292804c06868dfbaa31672add0eb74692adbb
jimipulsar <jimipulsar@gmail.com>
2019-03-06 12:56:11 +0100

    Completed

---
+++ app/Http/Controllers/AdmissionController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Lead;
+use App\Mail\SendNewLead;
+use Illuminate\Support\Facades\Mail;
+
+class AdmissionController extends Controller
+{
+    public function index(){
+      return view('admission.index');
+    }
+
+    public function save(Request $request){
+      $data = $request->all();
+
+      $newLead = new Lead();
+
+      $newLead->name = $data['name'];
+      $newLead->email = $data['email'];
+      $newLead->message = $data['message'];
+
+      $newLead->save();
+
+      $message = 'Congratulazioni! Messaggio inviato con successo.';
+
+      Mail::to('jimipulsar@gmail.com')->send(new SendNewLead($newLead));
+
+      return view('admission.index', compact('message'));
+    }
+}
---
+++ resources/views/mail/lead_mail.blade.php
@@ -0,0 +1,6 @@
+E' arrivata una nuova mail da: <br>
+{{$lead->name}} <br>
+La sua mail e': <br>
+{{$lead->email}} <br>
+Questo il suo messaggio:<br>
+{{$lead->message}}
